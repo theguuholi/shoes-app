fd1d51d93eca6a39f267b4209be4d8ea90002499
Gus <g.92oliveira@gmail.com>
2025-01-23 13:27:29 -0300

    install onesignal

--- App.tsx
+++ App.tsx
@@ -9,7 +9,10 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 import React from 'react';
+import { OneSignal } from 'react-native-onesignal';
 
+
+OneSignal.initialize("4eb4439c-de6a-447a-8f37-a6960537f847")
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 firebase.google.com
 onesignal>
 https://onesignal.com
+
+❯ npx expo install onesignal-expo-plugin@2.0.2
+❯ npm install react-native-onesignal@5.0.5
+
+https://dashboard.onesignal.com/apps/4eb4439c-de6a-447a-8f37-a6960537f847/settings/configure/google-android?newApp=true
